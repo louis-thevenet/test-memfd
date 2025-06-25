c229a74a85aba227137da317012065a82539906b
Louis Thevenet <louis.thevenet@nexedi.com>
2025-06-25 18:25:50 +0200

    feat: better print

--- main.c
+++ main.c
@@ -6,10 +6,16 @@
 
 int main() {
     #ifndef __NR_memfd_secret
-    printf("Doing stuff without memfd");
+        printf("Doing stuff without memfd\n");
     #else
-    printf("Doing stuff with memfd");
-    long ret = syscall(__NR_memfd_secret, 0);
+        printf("Doing stuff with memfd\n");
+        long ret = syscall(__NR_memfd_secret, 0);
+        if (ret == -1) {
+            perror("syscall memfd_secret failed\n");
+            return 1;
+        } else {
+        printf("syscall memfd_secret succeeded\n");
+        }
     #endif
 
     return 0;
