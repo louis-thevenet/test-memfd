c01f438ee3b5bcf3a76bff1c9936e595d5194b80
Louis Thevenet <louis.thevenet@nexedi.com>
2025-07-22 18:05:19 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -1,97 +1,23 @@
 {
   description = "Nixpkgs with older glibc overlay";
   inputs = {
-    # Latest stable nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/25.05";
-    # Older nixpkgs that glibc 2.31
-    # Easy to find with tools like https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=glibc
-    nixpkgs-2-31 = {
-      url = "github:NixOS/nixpkgs/3913f6a514fa3eb29e34af744cc97d0b0f93c35c";
-      flake = false;
-    };
-    nixpkgs-2-35 = {
-      url = "github:NixOS/nixpkgs/1b7a6a6e57661d7d4e0775658930059b77ce94a4";
-      flake = false;
-    };
   };
   outputs =
     {
       self,
       nixpkgs,
       ...
-    }@inputs:
+    }:
     let
       system = "x86_64-linux";
-      nixpkgs-old = inputs.nixpkgs-2-35;
-
-      pkgs-old = import nixpkgs-old { inherit system; };
-
-      glibcOverlay =
-        final: prev:
-        let
-          glibc =
-            (pkgs-old.glibc.override {
-              inherit (prev)
-                callPackage
-                ;
-            }).overrideAttrs
-              (attrs: {
-                configureFlags = attrs.configureFlags ++ [
-                  # new gcc has stricter error checking
-                  "--disable-werror"
-                ];
-                # https://stackoverflow.com/a/77107152 this glibc needs older make
-                depsBuildBuild = attrs.depsBuildBuild ++ [ prev.pkgsBuildBuild.gnumake42 ];
-                makeFlags = [ "OBJCOPY=${prev.stdenv.cc.targetPrefix}objcopy" ];
-                passthru = attrs.passthru // {
-                  libgcc = prev.libgcc;
-                };
-              })
-            // {
-              # new nixpkgs has this as a separate output
-              getent = glibc.bin;
-              stdenv = prev.stdenv // {
-                lib = glibc.lib;
-              };
-              pname = "glibc";
-            };
-        in
-        {
-          inherit glibc;
-          # inherit (nixpkgs-old) glibcIconv; # glibcLocales ;
-        };
-      pkgs-old-glibc = import nixpkgs {
+      pkgs = import nixpkgs {
         inherit system;
-        overlays = [ glibcOverlay ];
-        config.replaceStdenv =
-          { pkgs }:
-          pkgs.overrideCC pkgs.stdenv (
-            pkgs.wrapCCWith {
-              cc = pkgs.gcc-unwrapped.overrideAttrs (old: {
-                configureFlags = (old.configureFlags or [ ]) ++ [
-                  "--with-native-system-header-dir=${pkgs-old.glibc.dev}/include"
-                  "--with-glibc-version=${pkgs-old.glibc.version}"
-                ];
-              });
-              bintools = pkgs.wrapBintoolsWith {
-                bintools = pkgs.binutils-unwrapped;
-                libc = pkgs.glibc;
-              };
-              libc = pkgs.glibc;
-            }
-          );
-        config.replaceBootstrapFiles =
-          prevFiles:
-          (pkgs-old.callPackage "${nixpkgs-old}/pkgs/stdenv/linux/make-bootstrap-tools.nix" {
-            # localSystem = system;
-          }).bootstrapFiles;
       };
     in
     {
       packages.${system} = {
-        perl = pkgs-old-glibc.perl;
-        slapos-core = pkgs-old-glibc.callPackage ./slapos-core.nix { };
-        test-program = pkgs-old-glibc.stdenv.mkDerivation {
+        test-program = pkgs.stdenv.mkDerivation {
           name = "test-program";
           src = ./.;
           buildPhase = ''
--- nixpkgs/default.nix
+++
@@ -1,9 +0,0 @@
-{ inputs }:
-let
-  system = "x86_64-linux";
-in
-{
-  # pkgs-25-05 = import inputs.nixpkgs { inherit system; };
-  pkgs-2-31-recent = import ./nixpkgs-2-31-recent.nix { inherit inputs system; };
-  # pkgs-2-35-recent = import ./nixpkgs-2-35-recent.nix { inherit inputs system; };
-}
